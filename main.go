3daeafd080fd59286dcda2ae12c8ea6b736d8679
John Hughes <john.hughes@hurricanecommerce.com>
2023-07-12 18:12:50 +0100

    refactor: :recycle: bootstrap the database and iterator
    this is a crude implementation

--- main.go
+++ main.go
@@ -1,5 +1,77 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jonnaylang101/sql-iterator/database"
+	"github.com/jonnaylang101/sql-iterator/iterator"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	var err error
+
+	cfg := database.DbConfig{}
+	cfg.Host, err = Mustenv("HOST")
+	if err != nil {
+		return err
+	}
+
+	cfg.Port, err = Mustenv("PORT")
+	if err != nil {
+		return err
+	}
+
+	cfg.DbName, err = Mustenv("DB_NAME")
+	if err != nil {
+		return err
+	}
+
+	cfg.User, err = Mustenv("USER")
+	if err != nil {
+		return err
+	}
+
+	cfg.Password, err = Mustenv("PASSWORD")
+	if err != nil {
+		return err
+	}
+
+	db, err := database.New(cfg)
+	if err != nil {
+		return err
+	}
+
+	table, err := Mustenv("TABLE")
+	if err != nil {
+		return err
+	}
+
+	sentences, err := iterator.MakeSentencesFromDatabaseRows(db, table)
+	if err != nil {
+		return err
+	}
+
+	for _, sen := range sentences {
+		fmt.Println(sen)
+	}
+
+	return nil
+}
+
+func Mustenv(ev string) (string, error) {
+	t := os.Getenv(ev)
+	if t == "" {
+		return t, fmt.Errorf("missing env var = %s", ev)
+	}
+
+	return t, nil
 }
